c2bf89f3b3a92550f3887af573908fd1df6584c8
kkrachenfels <kkrachenf@gmail.com>
2024-11-13 16:40:48 -0800

    added form to create new item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,23 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(name: "...", price: 0, condition: "NEW", description: "...")
+
+    if @item.save
+      redirect_to @item
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     has_many :items, dependent: :destroy
     has_many :orders, dependent: :destroy
     has_many :reviews, dependent: :destroy
+
+    validates :name, presence: true, uniqueness: true
 end
