fae4e0738af79855bd3dbf04926e87901956d726
Ritu Kirsur <ritukirsur@gmail.com>
2024-11-19 09:30:25 -0800

    initial commit for orders page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,8 @@
 class OrdersController < ApplicationController
+    def index
+      @orders = Order.all
+    end
+
     def show
         @order = Order.find(params[:id])
         if @order.user_id != current_user.id
